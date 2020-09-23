1ce4aecfc0d729094f1516c7f4ffaf8bafa387c6
diegoavc12 <A00226803@itesm.mx>
2020-09-23 02:03:06 +0000

    practica 4 (incompleta)

---
+++ practicas/4/nomemates.c
@@ -0,0 +1,15 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+void signalHandler(int signum){
+    printf("jajaja no me matas\n");
+}
+
+int main(){
+    signal(2,signalHandler);
+    while(1){
+        sleep(2);
+        printf("trabajando\n");
+    }
+    return 0;
+}
